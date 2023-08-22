84141cb8b2b1a193abec9900d2e76f13dccd557a
Anwesha Chakraborty <anweshaofficial54@gmail.com>
2023-08-22 14:33:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+    "Which company owns the android?",
+            "Which of the following is not a programming language?",
+            "Who is the ceo of google?",
+            "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Mark Zuckerberg","Jeff Bezos","Sundar pichai","Tim Cook"},
+            {"Mark Zuckerberg","Jeff Bezos","Sundar pichai","Tim Cook"}
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Sundar pichai",
+            "Tim Cook"
+
+    };
+}
